2d72a4f94a073dc1037eab9cb3425a85dfccfab8
Stas Remnev <remnev.stas@gmail.com>
2022-01-19 22:32:22 +0300

    style: collapse

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,41 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const collapseBtn = document.querySelector('.collapsible__button');
+const collapseContent = document.querySelector('.collapsible__content');
+const collapseActionVisible = document.querySelector(
+  '.collapsible__action--visible'
+);
+const collapseActionHidden = document.querySelector(
+  '.collapsible__action--hidden'
+);
+
+let state = 'none';
+function toggle() {
+  if (state == 'none') {
+    collapseContent.style.display = 'block';
+    collapseActionVisible.style.display = 'block';
+    collapseActionHidden.style.display = 'none';
+    state = 'block';
+  } else {
+    collapseContent.style.display = 'none';
+    collapseActionVisible.style.display = 'none';
+    collapseActionHidden.style.display = 'block';
+    state = 'none';
+  }
+}
+
+collapseContent.style.display = 'none';
+collapseActionVisible.style.display = 'none';
+
+collapseBtn.addEventListener('click', toggle);
+
+collapseContent.animate(
+  [
+    { transform: 'scale(1)', opacity: 1 },
+    { transform: 'scale(.9)', opacity: 0.5 },
+  ],
+  {
+    duration: 1000,
+    easing: 'ease-in',
+    iterations: Infinity,
+    direction: 'alternate',
+  }
+);
